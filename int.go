3e9442674e6dfeda1edc46f4cec87db28ac08bee
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-09 12:08:55 +0900

    feature: Update int syntax
    feature: Update int syntax

---
+++ int.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var someName = "hello"
+
+func main() {
+
+	// ints
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// bits & memory
+	// var nameOne int16 = 25
+	// var numTwo int8 = -128
+	// var numThree int8 = 256
+
+	// fmt.Println(numOne, numTwo)
+
+}
