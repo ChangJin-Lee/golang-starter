06e70b7a184d079210c61bb1315f238e5541c752
ChangJin-Lee <ckdwls525@gmail.com>
2023-07-20 00:47:24 +0900

    update array in golang

---
+++ basicSyntax/array.go
@@ -0,0 +1,29 @@
+// go에서는 array란 특정 길이의 원소에 대한 인덱스가 정해진 시퀀스입니다.
+// 일반적으로 splice를 많이 사용합니다. 
+
+package main
+
+import "fmt"
+
+func main() {
+
+    var a [5]int
+    fmt.Println("emp:", a)
+
+    a[4] = 100
+    fmt.Println("set:", a)
+    fmt.Println("get:", a[4])
+
+    fmt.Println("len:", len(a))
+
+    b := [5]int{1, 2, 3, 4, 5}
+    fmt.Println("dcl:", b)
+
+    var twoD [2][3]int
+    for i := 0; i < 2; i++ {
+        for j := 0; j < 3; j++ {
+            twoD[i][j] = i + j
+        }
+    }
+    fmt.Println("2d: ", twoD)
+}
